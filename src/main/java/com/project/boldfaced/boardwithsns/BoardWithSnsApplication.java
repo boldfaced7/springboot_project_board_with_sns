d49c54d140d1b16d5b195653bf6a2d88d966f3fc
boldfaced7 <boldfaced753@gmail.com>
2023-12-14 19:47:54 +0900

    #3 - 오타 수정

--- src/main/java/com/project/boldfaced/boardwithsns/BoardWithSnsApplication.java
+++ src/main/java/com/project/boldfaced/boardwithsns/BoardWithSnsApplication.java
@@ -1,8 +1,10 @@
-package com.project.board_with_sns;
+package com.project.boldfaced.boardwithsns;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BoardWithSnsApplication {
 
